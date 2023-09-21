5729711f6e4c0c831cb73fa134363205503dbf37
Samanvith <Samanvith2005@gmail.com>
2023-09-21 11:27:51 +0530

    Store the Jwt token

---
+++ authentication/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'; // Updated import
+
+import MainProductsPage from './Components/ProductsPage'; // Remove '/components'
+import HomePage from './Components/HomePage';
+import LoginForm from './Components/LoginForm';
+import Cart from './Components/CartPage';
+import NotFound from './Components/NotFoundPage';
+import './App.css';
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />}/> 
+      <Route path="/login" element={<LoginForm />} />
+      <Route path="/products" element={<MainProductsPage />} /> {/* Updated path */}
+      <Route path="/cart" element={<Cart />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App;
---
+++ authentication/src/Components/ProductsPage/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../Header';
+import './index.css';
+
+const ProductsPage = () => (
+  <>
+    <Header />
+    <div className="products-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        alt="products"
+        className="products-img"
+      />
+    </div>
+  </>
+);
+
+export default ProductsPage;
